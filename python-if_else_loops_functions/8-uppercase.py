86ee77e2729cc04ff62714354c595f65a468fc05
gkarenzi-lang <your-github-email@example.com>
2026-06-30 10:42:32 +0200

    Fix 8-uppercase.py to use only 2 print calls

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,5 @@
 def uppercase(str):
     """Print a string in uppercase, followed by a newline."""
     for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
-    print()
+        print("{:c}".format(ord(c) - 32 if 97 <= ord(c) <= 122 else ord(c)), end="")
+    print("")
